8bc4dde3c4c9b9be5b5d3f17bb999071a97db231
João Vitor Sesma <23015160.maua@gmail.com>
2026-02-21 11:42:42 -0300

    feat(ap01-git) : implementa a soma

---
+++ ap01_git_introducao/calculadora.py
@@ -0,0 +1,13 @@
+# Funções
+## Função de Soma
+def somar (a, b):
+    return a + b
+
+# Main
+## Inputs
+v1 = int(input("Digite o Valor 1: "))
+v2 = int(input("Digite o Valor 2: "))
+
+## Testes
+soma = somar(v1, v2)
+print(f"Soma: {soma}")
