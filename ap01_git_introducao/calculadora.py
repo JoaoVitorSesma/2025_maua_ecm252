272e388fb945f898ab611a58f63955f2fb93cc41
João Vitor Sesma <23015160.maua@gmail.com>
2026-02-21 12:27:04 -0300

    feat(ap01-git) : implementa a subtracao

--- ap01_git_introducao/calculadora.py
+++ ap01_git_introducao/calculadora.py
@@ -3,11 +3,5 @@
 def somar (a, b):
     return a + b
 
-# Main
-## Inputs
-v1 = int(input("Digite o Valor 1: "))
-v2 = int(input("Digite o Valor 2: "))
-
-## Testes
-soma = somar(v1, v2)
-print(f"Soma: {soma}")
+def subtrair (a, b):
+    return a - b
