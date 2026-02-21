d27f0eed5f53e9621e02453d592ddb97a72ed1fb
João Vitor Sesma <23015160.maua@gmail.com>
2026-02-21 12:30:16 -0300

    test(ap01-git) : testa a funcao subtracao

--- ap01_git_introducao/teste_calculadora.py
+++ ap01_git_introducao/teste_calculadora.py
@@ -7,5 +7,10 @@ v1 = int(input("Digite o Valor 1: "))
 v2 = int(input("Digite o Valor 2: "))
 
 ## Testes
+### Soma
 soma = calculadora.somar(v1, v2)
-print(f"Somar {v1} + {v2} = {soma}")
+print(f"Somar: {v1} + {v2} = {soma}")
+
+### Subtração
+sub = calculadora.subtrair(v1, v2)
+print(f"Subtrair: {v1} + {v2} = {sub}")
