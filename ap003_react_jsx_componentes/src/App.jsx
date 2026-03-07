50f2aedcbb2d709309d6f4694460e2cc1a2e8397
João Vitor Sesma <jv0707204@gmail>
2026-03-07 10:42:40 -0300

    learn(ap03-react):encerra a apostila

--- ap003_react_jsx_componentes/src/App.jsx
+++ ap003_react_jsx_componentes/src/App.jsx
@@ -1,15 +1,36 @@
-const App = () => {
-  return <div style={{margin: 'auto', width: 576, backgroundColor: "wheat", padding: 12, borderRadius: 8}}>
-    <label htmlFor="campoNome" style={{display: 'block', marginBottom: 8}}>
-      Nome:
-    </label>
-    <input type="text" id="campoNome" style={{paddingTop: 8, paddingBottom: 8, width: '100%', borderStyle: 'hidden', outline: 'none', borderRadius: 8}}/>
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './style.css'
 
-    {/*button{Enviar}*/}
-    <button style = {{marginTop: 12, paddingTop: 8, paddingBottom: 8, backgroundColor: 'blue', borderStyle: 'hidden', color: 'whitesmoke', width: '100%', borderRadius: 8}}>
-      Enviar
-    </button>
-  </div>
+const App = () => {
+
+  const estiloBotao = {marginTop: 12, paddingTop: 8, paddingBottom: 8, backgroundColor: 'blueviolet', color: 'white', border: 'none', width: '100%',borderRadius: 8}
+
+  const textoDoRotulo = 'Nome:'
+
+  const obterTextoDoBotao = () => 'Enviar'
+
+  const aoClicar = () => alert('Clicou')
+  return <div style={{margin: 'auto', width: 768, backgroundColor: '#EEE', padding: 12, borderRadius: 8}}>
+
+  <label 
+  className="rotulo" htmlFor="nome" style={{display: 'block', marginBottom:4}}>
+  {textoDoRotulo}
+  </label>
+
+{/*input#campoNome*/}
+<input type="text" id="nome" style={{paddingTop: 8, paddingBottom: 8,
+borderStyle: 'hidden',width: '100%', borderRadius: 8, outline: 'none', boxSizing:
+'border-box'}}/>
+
+{/*button{Enviar}*/}
+<button
+  onClick = {aoClicar}
+  style={estiloBotao}>
+  {obterTextoDoBotao()}
+  </button>
+</div>
 }
 
 export default App
